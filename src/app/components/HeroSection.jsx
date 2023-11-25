13c5b8b04abadc3e5f6618ed530f9aac2cb0fc52
Russellyyds <954013384@qq.com>
2023-11-26 01:57:45 +1100

    update css

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,13 +4,34 @@ import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from "framer-motion";
 import Link from "next/link"
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 const HeroSection = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <section className="lg:py-16">
             <div className=" grid grid-cols-1 sm:grid-cols-12">
                 <motion.div initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.3 }} 
+                    transition={{ duration: 0.3 }}
                     className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className=" text-white mb-4 text-4xl sm:text-5xl lg:text-6xl
              font-extrabold">
@@ -39,25 +60,51 @@ const HeroSection = () => {
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptuous.
                     </p>
                     <div>
-                        <button className=" px-6 py-3 rounded-full w-full sm:w-fit mr-4 text-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-400">Hire Me</button>
-                        <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover: bg-slate-800 text-white border border-white mt-3">
-                            <span className=" block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                                <Link href="FullStack-CV-HangPan.pdf">Download CV</Link>
-                            </span>
+                        <button onClick={handleClickOpen} className=" px-6 py-3 rounded-full w-full sm:w-fit mr-4 text-white bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:bg-slate-400">
+                            Hire Me
                         </button>
+                        <Link href="FullStack-CV-HangPan.pdf"> 
+                            <button className="px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover: bg-slate-800 text-white border border-white mt-3">
+                                <span className=" block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                                    Download CV
+                                </span>
+                            </button>
+                        </Link>
                     </div>
                 </motion.div>
                 <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.3 }} 
-                className=" col-span-5 place-self-center mt-4 lg:mt-0">
+                    initial={{ opacity: 0, scale: 0.5 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.3 }}
+                    className=" col-span-5 place-self-center mt-4 lg:mt-0">
                     <div className=" rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
                         <Image className=" absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" src="/imgs/avatar.png" alt="my avatar" width={300} height={300} />
                     </div>
                 </motion.div>
             </div>
-
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle id="alert-dialog-slide-title">{"Please contact me"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description" color="primary">
+                        Email: Russell1997100188@gmail.com
+                    </DialogContentText>
+                    <DialogContentText id="alert-dialog-slide-description" color="primary">
+                        Phone: +61 0478223484
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        OK
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </section>
     );
 };
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
             <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href={'/'} className='text-2xl md:text-5xl text-white font-semibold'>
-                    Russell
+                    Russell Pan
                 </Link>
                 <div className='mobile-menu block md:hidden'>
                     {
@@ -41,8 +41,6 @@ const Navbar = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                                 </svg>
-
-
                             </button>
                     }
                 </div>
@@ -50,7 +48,7 @@ const Navbar = () => {
                     <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
                         {navLinks.map((link, index) => (
                             <li key={index}>
-                                <NavLink href={link.path}>{link.title}</NavLink>
+                                <NavLink href={link.path}><span className=' font-bold text-blue-300'>{link.title}</span></NavLink>
                             </li>
                         ))}
 
